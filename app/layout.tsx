91dbe956cee6d0eefc73548b86232d675e242735
Jeff <coulterjeff75@gmail.com>
2025-04-23 13:48:05 -0500

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { useRouter } from 'next/navigation';
 import "./globals.css";
 
 const geistSans = Geist({
